7f5c7e89e30356e518ed2dc085f6a2261db101ee
Alexandros Peppas <apeppas@microsoft.com>
2023-04-21 10:09:02 +0100

    lock and dispose

---
+++ SoftwareDesignPatterns/AsyncDisposable/LockSimulationObject.cs
@@ -0,0 +1,31 @@
+﻿namespace SoftwareDesignPatterns.AsyncDisposable
+{
+    public sealed class LockSimulationObject : IAsyncDisposable
+    {
+        public LockSimulationObject()
+        {
+            
+        }
+
+        public async Task DoSomethingThenDispose()
+        {
+            await using (this.ConfigureAwait(false))
+            {
+                for (int i=0; i<5; i++)
+                {
+                    Console.WriteLine($"Spending CPU cycles round {i}");
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+            }
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            Console.WriteLine("Disposing unmanaged resources...");
+
+            await Task.Delay(1000); // 1 sec
+
+            Console.WriteLine("Disposed!");
+        }
+    }
+}
---
+++ UnitTestsN/AsyncDisposable/DisposeObjectTests.cs
@@ -0,0 +1,21 @@
+﻿using SoftwareDesignPatterns.AsyncDisposable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.AsyncDisposable
+{
+    [TestClass]
+    public class DisposeObjectTests
+    {
+        [TestMethod]
+        public async Task DisposingObjectTests()
+        {
+            var disposableObject = new LockSimulationObject();
+
+            await disposableObject.DoSomethingThenDispose();
+        }
+    }
+}
