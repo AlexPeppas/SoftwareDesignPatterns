d3faa44b2c3cda658553450033d5ce6597a5012b
Alexandros Peppas <apeppas@microsoft.com>
2023-09-24 23:23:16 +0100

    Prototype pattern

---
+++ SoftwareDesignPatterns/Prototype Pattern/TestUser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDesignPatterns.Prototype_Pattern
+{
+    public abstract class Prototype
+    {
+        public abstract Prototype ShallowCopy();
+
+        public abstract Prototype DeepCopy();
+    }
+
+    public sealed class TestUser : Prototype
+    {
+        public string Name { get; set; }
+
+        public string[] Nicknames { get; set; } 
+
+        public Occupation Occupation { get; set; }
+
+        public override Prototype DeepCopy()
+        {
+            var clonedUser = (TestUser)this.MemberwiseClone();
+            clonedUser.Occupation = new Occupation { Level = Occupation.Level, Role = Occupation.Role };
+
+            clonedUser.Nicknames = new string[Nicknames.Length];
+            var indx = 0;
+            foreach (var nick in Nicknames)
+            {
+                clonedUser.Nicknames[indx++] = nick;
+            }
+
+            return clonedUser;
+        }
+
+        public override Prototype ShallowCopy()
+        {
+            return (Prototype)this.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {string.Join(",", Nicknames)}, {Occupation.Role}-{Occupation.Level}";
+        }
+    }
+
+    public sealed class Occupation
+    {
+        public string Role { get; set; }
+
+        public int Level { get; set; }
+    }
+
+    public sealed class PrototypeManager
+    {
+        private readonly Dictionary<string, Prototype> protoStore= new();
+
+        public Prototype this[string key]
+        {
+            get => protoStore[key];
+            set => protoStore[key] = value;
+        }
+    }
+}
---
+++ UnitTestsN/Prototype Pattern/TestUserPrototypeTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SoftwareDesignPatterns.Prototype_Pattern;
+
+namespace UnitTests.Prototype_Pattern
+{
+    [TestClass]
+    public sealed class TestUserPrototypeTests
+    {
+        [TestMethod]
+        public void CompareCopies()
+        {
+            var user = new TestUser { Name = "Alex", Nicknames = new string[2] { "Ale", "Alexandros" }, Occupation = new Occupation { Role = "SDE", Level = 62 } };
+
+            var clonedUser = user.ShallowCopy();
+            var deepCopy = user.DeepCopy();
+
+            Debug.WriteLine("\n User: \n" + user.ToString());
+            Debug.WriteLine("\n ClonedUser: \n" + clonedUser.ToString());
+            Debug.WriteLine("\n DeepCopyUser: \n" + deepCopy.ToString());
+
+            user.Nicknames[0] = "Peppas";
+            user.Occupation.Level = 63;
+
+            Debug.WriteLine("MODIFIED INITIAL USER");
+            Debug.WriteLine("---------------------");
+
+            Debug.WriteLine("\n User: \n" + user.ToString());
+            Debug.WriteLine("\n ClonedUser: \n" + clonedUser.ToString());
+            Debug.WriteLine("\n DeepCopyUser: \n" + deepCopy.ToString());
+        }
+
+        [TestMethod]
+        public void PrototypeManagerTests()
+        {
+            var protoManager = new PrototypeManager();
+            var user = new TestUser { Name = "Alex", Nicknames = new string[2] { "Ale", "Alexandros" }, Occupation = new Occupation { Role = "SDE", Level = 62 } };
+
+            protoManager["user"] = user;
+            protoManager["clone"] = user.ShallowCopy();
+            protoManager["deepCopy"] = user.DeepCopy();
+
+            Debug.WriteLine("\n User: \n" + user.ToString());
+            Debug.WriteLine("\n ClonedUser: \n" + protoManager["user"].ToString());
+            Debug.WriteLine("\n DeepCopyUser: \n" + protoManager["deepCopy"].ToString());
+
+            user.Nicknames[0] = "Peppas";
+            user.Occupation.Level = 63;
+
+            Debug.WriteLine("MODIFIED INITIAL USER");
+            Debug.WriteLine("---------------------");
+
+            Debug.WriteLine("\n User: \n" + user.ToString());
+            Debug.WriteLine("\n ClonedUser: \n" + protoManager["user"].ToString());
+            Debug.WriteLine("\n DeepCopyUser: \n" + protoManager["deepCopy"].ToString());
+        }
+    }
+}
